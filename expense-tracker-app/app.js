9fc908ba170a496535d6409aab0d7c6391580c16
hassan haroon <hassanharoon321@gmail.com>
2021-03-04 03:01:58 +0500

    added

--- expense-tracker-app/app.js
+++ expense-tracker-app/app.js
@@ -1,5 +1,6 @@
 let balance = document.getElementById("income");
 let balanceValue; //balance
+
 const addIncome = () => {
   if (balance.value === "") {
     alert("Please Input Some Value")
@@ -34,46 +35,64 @@ const addExpense = () => {
 
     //create tr (Single)
     var tr = document.createElement("tr");
-    allElement.appendChild(tr)
 
-    //Craete td for date (1st Child)
-    var tdDate = document.createElement("td");
-    var tdDateText = document.createTextNode(dateValue);
-    tdDate.appendChild(tdDateText);
-    allElement.appendChild(tdDate);
+
+    //Craete td for Amount (1st Child)
+    var tdAmount = document.createElement("td");
+    var tdAmountText = document.createTextNode(expenseValue);
+    tdAmount.appendChild(tdAmountText);
+    tr.appendChild(tdAmount);
 
     //Craete td for Category (2st Child)
     var tdCategory = document.createElement("td");
     var tdCategoryText = document.createTextNode(optionValue);
     tdCategory.appendChild(tdCategoryText);
-    allElement.appendChild(tdCategory);
+    tr.appendChild(tdCategory)
 
-    //Craete td for Amount (3st Child)
-    var tdAmount = document.createElement("td");
-    var tdAmountText = document.createTextNode(expenseValue);
-    tdAmount.appendChild(tdAmountText);
-    allElement.appendChild(tdAmount);
+    //Craete td for date (3st Child)
+    var tdDate = document.createElement("td");
+    var tdDateText = document.createTextNode(dateValue);
+    tdDate.appendChild(tdDateText);
+    tr.appendChild(tdDate);
 
     //Craete edit button (4st Child)
     var tdEdit = document.createElement("td")
     var editBtn = document.createElement("button")
     var editBtnText = document.createTextNode("Edit")
     editBtn.appendChild(editBtnText);
-    tdEdit.appendChild(editBtn)
-    allElement.appendChild(tdEdit)
+    tdEdit.appendChild(editBtn);
+    editBtn.setAttribute("onclick", "EditItem(this)")
+    tr.appendChild(tdEdit);
 
     //Craete delete button (4st Child)
     var tdDelete = document.createElement("td")
     var deleteBtn = document.createElement("button")
     var deleteBtnText = document.createTextNode("Delete")
     deleteBtn.appendChild(deleteBtnText);
+    deleteBtn.setAttribute("onclick", "deleteItem(this)")
     tdDelete.appendChild(deleteBtn)
-    allElement.appendChild(deleteBtn)
+    tr.appendChild(tdDelete);
+
+    allElement.appendChild(tr)
   }
+}
 
+function deleteItem(e) {
+  var currentBalance = document.getElementById("currentBalance");
+  e.parentNode.parentNode.remove();
+  var newExpense = e.parentNode.parentNode.firstChild.innerText;
+  var expense = parseInt(newExpense)
 
+  currentBalance.innerHTML = balanceValue += expense;
+}
 
-
+function EditItem(e) {
+  var currentBalance = document.getElementById("currentBalance");
+  var newExpense = prompt("Enter New Value");
+  var expense = parseInt(newExpense);
+  e.parentNode.parentNode.firstChild.innerText = expense;
+  console.log(currentBalance);
+  currentBalance.innerHTML = balanceValue += expense 
 }
 
 
@@ -99,90 +118,3 @@ const addExpense = () => {
 
 
 
-
-
-
-
-
-// var income = document.getElementById("income")
-// var id = 0;
-// const addIncome = () => {
-//   var currentBalance = document.getElementById("currentBalance");
-//   currentBalance.innerHTML += income.value;
-// }
-// const addExpense = () => {
-//   var expenses = [];
-//   var kharcha = [];
-//   id = ++id;
-//   var amountInput = document.getElementById("amountInput").value;
-//   var amountDate = document.getElementById("amountDate").value;
-//   var amountOption = document.getElementById("amountOption").value;
-//   var allElement = document.getElementById("allElement");
-
-//   //something weird here dont cross border (start)
-
-//   var expenseObj = {
-//     id: id,
-//     expense: amountInput,
-//     date: amountDate,
-//     categories: amountOption
-//   }
-
-//   expenses.push(expenseObj);
-
-//   var totalIncome = parseInt(income.value);  //paisa
-
-//   expenses.forEach((obj) => {
-//     var totalExpense = parseInt(obj.expense); //kharcha
-//     kharcha.push(totalExpense);
-//     console.log(kharcha);
-//     var hello = 0;
-//     for (var i = 0; i <= kharcha.length; i++) {
-//       hello += kharcha[i];
-//       console.log(hello)
-//     }
-//   })
-
-//   //something weird here dont cross border (end)
-
-//   //create tr (Single)
-//   var tr = document.createElement("tr");
-//   allElement.appendChild(tr)
-
-//   //Craete td for date (1st Child)
-//   var tdDate = document.createElement("td");
-//   var tdDateText = document.createTextNode(amountDate);
-//   tdDate.appendChild(tdDateText);
-//   allElement.appendChild(tdDate);
-
-//   //Craete td for Category (2st Child)
-//   var tdCategory = document.createElement("td");
-//   var tdCategoryText = document.createTextNode(amountOption);
-//   tdCategory.appendChild(tdCategoryText);
-//   allElement.appendChild(tdCategory);
-
-//   //Craete td for Amount (3st Child)
-//   var tdAmount = document.createElement("td");
-//   var tdAmountText = document.createTextNode(amountInput);
-//   tdAmount.appendChild(tdAmountText);
-//   allElement.appendChild(tdAmount);
-
-//   //Craete edit button (4st Child)
-//   var tdEdit = document.createElement("td")
-//   var editBtn = document.createElement("button")
-//   var editBtnText = document.createTextNode("Edit")
-//   editBtn.appendChild(editBtnText);
-//   tdEdit.appendChild(editBtn)
-//   allElement.appendChild(tdEdit)
-
-//   //Craete delete button (4st Child)
-//   var tdDelete = document.createElement("td")
-//   var deleteBtn = document.createElement("button")
-//   var deleteBtnText = document.createTextNode("Delete")
-//   deleteBtn.appendChild(deleteBtnText);
-//   tdDelete.appendChild(deleteBtn)
-//   allElement.appendChild(deleteBtn)
-
-
-
-// }
