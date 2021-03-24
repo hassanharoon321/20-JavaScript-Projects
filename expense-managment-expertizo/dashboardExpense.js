4eaab8f41b7a7f3a1070cd75c21db633df561790
hassan haroon <hassanharoon321@gmail.com>
2021-03-24 13:25:08 +0500

    added

--- expense-managment-expertizo/dashboardExpense.js
+++ expense-managment-expertizo/dashboardExpense.js
@@ -1,58 +1,58 @@
 let expenseItem = localStorage.getItem('expenseItem');
 expenseItem = JSON.parse(expenseItem);
 
-console.log(expenseItem);
-
 const tBody = document.getElementById("table-expense-body");
 
-expenseItem.map( (item)=>{
+expenseItem.map((item) => {
 
     const tr = document.createElement("tr");
 
-const thId = document.createElement("th");
-const thIdText = document.createTextNode(item.id);
-thId.appendChild(thIdText)
-tr.appendChild(thId)
+    const thId = document.createElement("th");
+    const thIdText = document.createTextNode(item.id);
+    thId.appendChild(thIdText)
+    tr.appendChild(thId)
 
-const thDescription = document.createElement("th");
-const thDescriptionText = document.createTextNode(item.description);
-thDescription.appendChild(thDescriptionText);
-tr.appendChild(thDescription)
+    const thDescription = document.createElement("th");
+    const thDescriptionText = document.createTextNode(item.description);
+    thDescription.appendChild(thDescriptionText);
+    tr.appendChild(thDescription)
 
-const thAmount = document.createElement("th");
-const thAmountText = document.createTextNode(item.amount);
-thAmount.appendChild(thAmountText);
-tr.appendChild(thAmount);
+    const thAmount = document.createElement("th");
+    const thAmountText = document.createTextNode(item.amount);
+    thAmount.appendChild(thAmountText);
+    tr.appendChild(thAmount);
 
-const thCategory = document.createElement("th");
-const thCategoryText = document.createTextNode(item.category);
-thCategory.appendChild(thCategoryText);
-tr.appendChild(thCategory);
+    const thCategory = document.createElement("th");
+    const thCategoryText = document.createTextNode(item.category);
+    thCategory.appendChild(thCategoryText);
+    tr.appendChild(thCategory);
 
-const thDate = document.createElement("th");
-const thDateText = document.createTextNode(item.createdOn);
-thDate.appendChild(thDateText);
-tr.appendChild(thDate);
+    const thDate = document.createElement("th");
+    const thDateText = document.createTextNode(item.createdOn);
+    thDate.appendChild(thDateText);
+    tr.appendChild(thDate);
 
-const thActions = document.createElement("th");
+    const thActions = document.createElement("th");
 
-const editBtn = document.createElement('button');
-const editBtnText = document.createTextNode("Edit");
-editBtn.setAttribute("class","btn btn-primary")
-editBtn.appendChild(editBtnText);
+    const editBtn = document.createElement('button');
+    const editBtnText = document.createTextNode("Edit");
+    editBtn.setAttribute("class", "btn btn-primary")
+    editBtn.appendChild(editBtnText);
 
-const deleteBtn = document.createElement('button');
-const deleteBtnText = document.createTextNode("Delete");
-deleteBtn.setAttribute("class","btn btn-danger")
-deleteBtn.appendChild(deleteBtnText);
+    const deleteBtn = document.createElement('button');
+    const deleteBtnText = document.createTextNode("Delete");
+    deleteBtn.setAttribute("class", "btn btn-danger")
+    deleteBtn.appendChild(deleteBtnText);
 
-thActions.appendChild(editBtn)
-thActions.appendChild(deleteBtn)
+    thActions.appendChild(editBtn)
+    thActions.appendChild(deleteBtn)
 
-tr.appendChild(thActions)
+    tr.appendChild(thActions)
+
+    tBody.appendChild(tr);
+
+})
 
 
-tBody.appendChild(tr);
 
-} )
 
